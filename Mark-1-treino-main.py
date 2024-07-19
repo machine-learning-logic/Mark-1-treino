074c5d41076be916114ccd313c1bba26a2777687
machine-learning-logic <anquelvinique@gmail.com>
2024-07-19 20:20:26 -0300

    backprop

--- Mark-1-treino-main.py
+++ Mark-1-treino-main.py
@@ -2,11 +2,18 @@ import numpy as np
 
 a = lambda x, y: abs(x-y)
 
-def inferir(entrada, pesos, vises):
 
-	return np.dot(entrada, pesos)+vieses
-	
-def retro(entrada, vieses, saida, gabarito):
-	
-	e = a(saida, gabarito)
-	return (np.array(entrada[0]*e[0], entrada[1]*e[1]), e)
+def retro(entrada, pesos, vieses, gabarito):
+
+	e = a(np.dot(entrada, pesos) + vieses, gabarito)
+	r_par = np.array([entrada * e[0], entrada * e[1], e])
+	t_aprendizado = 0.01
+	t_pesos = np.zeros((2, 12))
+	t_vieses = np.zeros(2)
+
+	def intera():
+
+		e = a(np.dot(entrada, pesos) + vieses, gabarito)
+		r_par = np.array([entrada * e[0], entrada * e[1], e])
+		t_pesos += t_aprendizado
+		t_vieses += t_aprendizado
