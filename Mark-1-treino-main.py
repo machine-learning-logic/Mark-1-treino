ba17d61e04a7fefb3da494a59d3fb3394bc3c21c
machine-learning-logic <anquelvinique@gmail.com>
2024-07-24 00:02:58 -0300

    Funções simplificadas

--- Mark-1-treino-main.py
+++ Mark-1-treino-main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from amostras import detaset
 
 a = lambda x, y: abs(x-y)
 b = lambda x, y: 1 if x < y else -1
@@ -26,4 +27,17 @@ def interacao(entrada, pesos, vieses, gabarito,
 
 def ephoc(carac, tipo):
 
-	
+	p_0 = np.random.default_rng().random((2, 12))
+	v_0 = np.array([0.1, -0.2])
+	e_0 = np.random.default_rng().random((26,))
+	s_0 = np.zeros(26)
+	a_0 = np.zeros(26)
+	val_tr = np.zeros(1500)
+
+	for i in dataset[0:1501]:
+
+		estado = interacao(i[0:12], p_0, v_0, i[12], e_0, s_0, a_0)
+		p_0, v_0 = estado[], estado[]
+		e_0, s_0, a_0 = estado[], estado[], estado[]
+
+	return tuple(p_0, v_0, val_tr)
