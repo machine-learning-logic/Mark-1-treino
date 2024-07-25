bc1fd3a541aa760c21083979b8fc2fb4459d74e2
machine-learning-logic <anquelvinique@gmail.com>
2024-07-25 07:57:18 -0300

    Melhor formatação dos resultados

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from M_1_treino import ephoc
 import amostras
 
 arrays = ephoc()
+p = str([list(i) for i in arrays[0]])
+v = str([list(i) for i in arrays[1]])
 txt = f"pesos = {p}\nvieses = {v}erro  = {e}"
 open("resultados.py", "w").write(txt)
