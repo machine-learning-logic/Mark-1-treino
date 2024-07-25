ef22c6a46010efbc2651fd2a9ba814acd9823af7
machine-learning-logic <anquelvinique@gmail.com>
2024-07-25 09:12:29 -0300

    Erro ok

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import amostras
 
 arrays = ephoc()
 p = str([list(i) for i in arrays[0]])
-v = str([list(i) for i in arrays[1]])
+v = str(list(arrays[1]))
 txt = f"pesos = {p}\nvieses = {v}\nerro  = {str(list(e))}"
 open("resultados.py", "w").write(txt)
